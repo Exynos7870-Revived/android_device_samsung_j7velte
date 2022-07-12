7c5bf00e784a1a7fe056333cdb06ed66f691485a
FlominatorGD <github.flominatorgd@gmail.com>
2022-07-12 18:31:30 +0200

    declare use of oss audio and uprev to android.hardware.soundtrigger@2.3-impl, android.hardware.audio.effect@6.0-impl, android.hardware.audio@6.0-impl, add device speciffic wifi overlay, add fix for hotspot on android 11 for qcom wifi by setting WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true, add missing android.hardware.nfc.uicc.xml, Add parameters for Hotspot 2.0 in wpa_supplicant_overlay.conf, refractor for arrow

--- device.mk
+++ device.mk
@@ -16,6 +16,14 @@
 
 DEVICE_PATH := device/samsung/j7velte
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+
 # Custom mixer_paths
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
@@ -41,8 +49,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
-   
-PRODUCT_COPY_FILES += \  
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # NFC
@@ -81,6 +89,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal \
     libwpa_client \
+		WifiOverlay \
     wificond \
     wifiloader \
     wifilogd \
@@ -92,10 +101,27 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Custom mixer_paths OSS
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/configs/audio/oss/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+
+# Inherit from common (audio)
+$(call inherit-product, device/samsung/universal7870-common/device-oss_audio.mk)
+
+# Custom mixer_paths prebuilt
+#PRODUCT_COPY_FILES += \
+#    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+#    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+
+# Inherit from common
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_audio.mk)
+
 # Properties
 -include $(DEVICE_PATH)/system_prop.mk
 
 # Inherit from common
 $(call inherit-product, device/samsung/universal7870-common/device-common.mk)
+
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_bsp-vndk.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/j7velte/j7velte-vendor.mk)
